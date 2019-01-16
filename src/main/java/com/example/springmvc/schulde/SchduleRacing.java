55f7ed5351aec080f4d5e05d6e58c415b7995540
backsu83 <100seunghyun@naver.com>
2019-01-16 22:25:18 +0900

    schdule add localdate format

--- src/main/java/com/example/springmvc/schulde/SchduleRacing.java
+++ src/main/java/com/example/springmvc/schulde/SchduleRacing.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class SchduleRacing {
 
     @Autowired
-    private DisplayBoardService board;
+    private DisplayBoardService racing;
 
     @Scheduled(cron = "0 0 9 * * *")
     public void schduleAddOdds() {
-        board.addOddsForBatch("20190105");
+        racing.addOddsForBatch(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
--- src/test/java/com/example/springmvc/service/DisplayBoardServiceTest.java
+++ src/test/java/com/example/springmvc/service/DisplayBoardServiceTest.java
@@ -1,7 +1,7 @@
 package com.example.springmvc.service;
 
-import com.example.springmvc.dto.Odds;
 import com.example.springmvc.dao.database.OddsRepository;
+import com.example.springmvc.dto.Odds;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +25,6 @@ public class DisplayBoardServiceTest {
 
     @Test
     public void testLocalDate() {
-        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
